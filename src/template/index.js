37f47145f5016b43e21c3a6c855abcf74b1a176a
raphamorim <rapha850@gmail.com>
2017-02-11 01:25:30 -0200

    use elekid 0.2.0

--- src/template/index.js
+++ src/template/index.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import fs from 'fs'
-import { renderToString } from 'react-dom/server'
-import App  from '../containers/App'
-import template from './template'
+module.exports = (app) => {
+    return `<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-const appString = renderToString(<App/>)
-fs.writeFileSync(`${process.cwd()}/index.html`, template(appString), 'utf-8')
+        <title>Retro</title>
+        <link rel="stylesheet" href="assets/stylesheet/fonts.css">
+        <link rel="stylesheet" href="assets/stylesheet/base.css">
+        <link rel="stylesheet" href="assets/stylesheet/modal.css">
+
+        <script>
+            /* Events */
+            const saveEv = new Event('save-file');
+        </script>
+
+        <script src="src/ace/ace.js"></script>
+        <script src="src/ace/ext-language_tools.js"></script>
+        <script src="src/ace/ext-emmet.js"></script>
+        <script src="assets/javascript/keymaster.js"></script>
+    </head>
+    <body>
+        <div id="root">${app}</div>
+        <script async src="dist/retro.js"></script>
+    </body>
+</html>`
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+'use strict';
+
 const webpack = require('webpack')
 const path = require('path')
 const fs = require('fs')
-const WebpackShellPlugin = require('webpack-shell-plugin')
 
 const nodeEnv = process.env.NODE_ENV || 'development'
 const isProd = nodeEnv === 'production'
@@ -12,7 +13,7 @@ fs.readdirSync('node_modules')
 	.filter((x) => ['.bin'].indexOf(x) === -1)
 	.forEach((mod) => nodeModules[mod] = 'commonjs ' + mod)
 
-module.exports = [{
+module.exports = {
 	target: 'electron',
 	devtool: isProd ? 'source-map' : 'cheap-eval-source-map',
 	context: __dirname + '/src',
@@ -47,33 +48,3 @@ module.exports = [{
 		})
 	]
 }
-// , {
-// 	target: 'node',
-// 	context: __dirname + '/src',
-// 	entry: [
-// 		'./template/index.js'
-// 	],
-// 	output: {
-// 		path: __dirname + '/dist',
-// 		filename: 'template.js'
-// 	},
-// 	module: {
-// 		loaders: [{
-// 			test: /.js?$/,
-// 			loader: 'babel-loader',
-// 			exclude: /node_modules/,
-// 			query: {
-// 				presets: ['es2015', 'react']
-// 			}
-// 		}]
-// 	},
-// 	plugins: [
-// 		new WebpackShellPlugin({
-// 			onBuildStart: [ 'echo "Building index.html"' ],
-// 			onBuildEnd: [ 'node ./dist/template.js', 'echo "Builded index.html"' ]
-// 		})
-// 	],
-// 	externals: nodeModules,
-// 	devtool: 'sourcemap',
-// }
-]
